099c29cc58005042470330863b7d938645bb362a
Jignesh Makvana <jigneshmakvana@gmail.com>
2026-02-04 17:46:02 +0530

    fix: update middleware to allow access to static assets in public folder

--- src/middleware.js
+++ src/middleware.js
@@ -29,7 +29,8 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
+         * - any files with extensions (e.g. logo.png)
          */
-        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+        "/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)",
     ],
 };
